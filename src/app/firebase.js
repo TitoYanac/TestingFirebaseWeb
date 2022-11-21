102d0aaf0a8f033298ce2f337f99c94d955010b5
tito jesús <titoyanac@gmail.com>
2022-11-21 15:14:36 -0500

    sin credenciales

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -5,12 +5,6 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase
 
 
   const firebaseConfig = {
-        apiKey: "AIzaSyAvMlR70XR4nTStGkDaEEurRW2RwMzsTsE",
-        authDomain: "dbtesting-c9107.firebaseapp.com",
-        projectId: "dbtesting-c9107",
-        storageBucket: "dbtesting-c9107.appspot.com",
-        messagingSenderId: "603681291830",
-        appId: "1:603681291830:web:8a20a15b9141cf9d04e297"
     };
 
   // Initialize Firebase
